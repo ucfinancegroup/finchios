e1551baadcc5ac540f432b4b4006a50834d38fe3
Brett Fazio <brett.fazio@hotmail.com>
2020-12-18 20:09:13 -0500

    pie view swift wrapper

---
+++ finchios/Presentation/ViewRepresentable/Charts/PieView.swift
@@ -0,0 +1,45 @@
+//
+//  PieView.swift
+//  finchios
+//
+//  Created by Brett Fazio on 12/18/20.
+//
+
+import Charts
+import SwiftUI
+
+struct PieView: UIViewRepresentable {
+    typealias UIViewType = PieChartView
+    
+    var entries: [PieChartDataEntry]
+
+    func makeUIView(context: Context) -> PieChartView {
+        let view = PieChartView()
+        view.data = addData()
+        return view
+    }
+    
+    func addData() -> PieChartData {
+        let data = PieChartData()
+        let dataset = PieChartDataSet(entries: entries)
+        
+        dataset.colors = [.red, .blue, .green, .purple, .orange]
+        
+        data.addDataSet(dataset)
+        
+        return data
+    }
+    
+    func updateUIView(_ uiView: PieChartView, context: Context) {
+        
+    }
+}
+
+struct PieViewPreview: PreviewProvider {
+    static var previews: some View {
+        PieView(entries: [
+            PieChartDataEntry(value: 5, label: "five"),
+            PieChartDataEntry(value: 10, label: "ten")
+        ])
+    }
+}
