87d6b5b4fcc32298e73ec53a9a8d43e86e1d6665
Brett Fazio <brett.fazio@hotmail.com>
2020-12-28 20:55:06 -0500

    add UIColor extension for teal

---
+++ finchios/Extensions/UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor.swift
+//  finchios
+//
+//  Created by Brett Fazio on 12/28/20.
+//
+
+import SwiftUI
+
+extension UIColor {
+    
+    static var teal: UIColor = UIColor(red: 63/255.0, green: 165/255.0, blue: 164/255.0, alpha: 1.0)
+    
+}
