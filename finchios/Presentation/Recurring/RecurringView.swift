fc28351ba76ce0d02cb947a397544a1aad7b9517
Brett Fazio <brett.fazio@hotmail.com>
2021-01-08 17:50:34 -0500

    change away from template text for compilation

--- finchios/Presentation/Recurring/RecurringView.swift
+++ finchios/Presentation/Recurring/RecurringView.swift
@@ -20,7 +20,7 @@ struct RecurringView: View {
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(type.rawValue)
     }
 }
 
