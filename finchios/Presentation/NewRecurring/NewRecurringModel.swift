0d871e19915706494f6fca678a5dc2dd3a9d7ca4
Brett Fazio <brett.fazio@hotmail.com>
2021-01-11 18:48:58 -0500

    create function for new recurring

--- finchios/Presentation/NewRecurring/NewRecurringModel.swift
+++ finchios/Presentation/NewRecurring/NewRecurringModel.swift
@@ -28,5 +28,39 @@ class NewRecurringModel: ObservableObject, Identifiable {
     
     func create() {
         
+        // Create a payload and pass it to the service.
+        // Just need to ensure all ints / doubles are valid and non-empty.
+        
+        // TODO(): If the amount is non-empty, fill the principal & interest as 0.
+        
+        // TODO(): Do the same for amount.
+        
+        let timeInterval = TimeInterval(typ: .monthly, content: 5)
+        
+        let payload = RecurringNewPayload(name: name,
+                                          start: Int64(start.timeIntervalSince1970),
+                                          end: Int64(start.timeIntervalSince1970),
+                                          principal: 0,
+                                          amount: 0,
+                                          interest: 0.0,
+                                          frequency: timeInterval)
+        
+        RecurringService.newRecurring(payload: payload) { (success, error) in
+            DispatchQueue.main.async {
+                self.errorString = ""
+                
+                if success {
+                    // TODO(): Dismiss
+                    // Potentionally show a success alert and then from there set the presentation bool to false.
+                }else {
+                    if let error = error {
+                        self.errorString = "\(error)"
+                    }
+                    self.showError = true
+                }
+            }
+
+        }
+        
     }
 }
