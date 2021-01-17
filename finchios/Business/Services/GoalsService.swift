78612d35171e91c4b12529b558339310f4378ec0
Brett Fazio <brett.fazio@hotmail.com>
2021-01-17 10:51:31 -0500

    new goal service func

--- finchios/Business/Services/GoalsService.swift
+++ finchios/Business/Services/GoalsService.swift
@@ -48,3 +48,53 @@ struct GoalsService {
     }
     
 }
+
+// POST goal/new
+extension GoalsService {
+    
+    public static func newGoal(payload: GoalNewPayload, completion: @escaping ((Bool, Error?, GoalAndStatus?) -> Void)) {
+        guard let url = getNewGoalURL() else {
+            completion(false, nil, nil)
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        request.allHTTPHeaderFields = ["Content-Type": "application/json",
+                                       BusinessConstants.SET_COOKIE : CredentialsObject.shared.jwt]
+
+        let jsonBody = try? JSONEncoder().encode(payload)
+
+        guard let unwrappedJsonBody = jsonBody else {
+            completion(false, nil, nil)
+            return
+        }
+
+        request.httpBody = unwrappedJsonBody
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, urlResponse, error) in
+            guard let data = data else {
+                completion(false, error, nil)
+                return
+            }
+            
+            guard let response = try? JSONDecoder().decode(GoalAndStatus.self, from: data) else {
+                completion(false, error, nil)
+                return
+            }
+            
+            completion(true, nil, response)
+            return
+        }
+
+        task.resume()
+    }
+    
+    private static func getNewGoalURL() -> URL? {
+        let address = "\(BusinessConstants.SERVER)/goal/new"
+        
+        return URL(string: address)
+    }
+    
+}
