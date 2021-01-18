3f10140c34d0d3099861bbf36b9e44af18929b0d
Brett Fazio <brett.fazio@hotmail.com>
2021-01-18 16:53:25 -0500

    goal edit stub

---
+++ finchios/Presentation/Goal/GoalEdit/GoalEditView.swift
@@ -0,0 +1,89 @@
+//
+//  GoalEditView.swift
+//  finchios
+//
+//  Created by Brett Fazio on 1/18/21.
+//
+
+import SwiftUI
+import OpenAPIClient
+
+struct GoalEditView: View {
+    
+    @Binding var present: Bool
+    
+    @Binding var goal: GoalAndStatus
+    
+    @ObservedObject var model: GoalEditViewModel = GoalEditViewModel()
+    
+    var body: some View {
+        VStack {
+            
+            Group {
+                
+                Spacer()
+                
+                TextField("Name", text: self.$model.name)
+                
+                Spacer()
+                
+                DatePicker("Start", selection: self.$model.start)
+                
+                Spacer()
+                
+                DatePicker("End", selection: self.$model.end)
+                
+                Spacer()
+                
+                TextField("Target", text: self.$model.threshold)
+                    .keyboardType(.numberPad)
+                
+                Spacer()
+                
+                Picker("Metric", selection: self.$model.metric) {
+                    ForEach(GoalMetricIdentifiable.allCases, id: \.id) { type in
+                        Text(type.rawValue.capitalized)
+                    }
+                }
+                
+            }
+            
+            Spacer()
+            
+            Button {
+                self.model.edit(id: goal.goal.id.oid)
+            } label: {
+                Text("Save Edit")
+            }
+            
+        }
+        // TODO(): Not showing because it is a modal sheet?
+        .alert(isPresented: $model.showAlert) { () -> Alert in
+            if model.alertType == .fail {
+                return Alert(title: Text("Failed to edit"),
+                             message: Text("Failed to edit the goal. Error: \(self.model.errorDetail)"),
+                             dismissButton: .destructive(Text("Okay")) {
+                                self.model.showAlert = false
+                             })
+            }
+            else { // success
+                return Alert(title: Text("Success!"),
+                             message: Text("This goal has been successfully edited."),
+                             dismissButton: .default(Text("Okay")) {
+                                self.present = false
+                             })
+            }
+        }
+        .padding()
+        .onAppear() {
+            self.model.set(goal: self.goal)
+        }
+
+    }
+}
+
+//struct RecurringEditView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        RecurringEditView()
+//    }
+//}
---
+++ finchios/Presentation/Goal/GoalEdit/GoalEditViewModel.swift
@@ -0,0 +1,112 @@
+//
+//  GoalEditViewModel.swift
+//  finchios
+//
+//  Created by Brett Fazio on 1/18/21.
+//
+
+import Foundation
+import OpenAPIClient
+
+class GoalEditViewModel: ObservableObject, Identifiable {
+    
+    @Published var showAlert: Bool = false
+    @Published var alertType: SuccessFail = .success
+    @Published var errorDetail: String = ""
+    
+    // Fields
+    @Published var name: String = ""
+    @Published var start: Date = Date()
+    @Published var end: Date = Date()
+    @Published var threshold: String = ""
+    @Published var metric: GoalMetricIdentifiable = .savings
+    
+    func set(goal: GoalAndStatus) {
+        
+        name = goal.goal.name
+        start = Date(timeIntervalSince1970: TimeInterval(goal.goal.start))
+        end = Date(timeIntervalSince1970: TimeInterval(goal.goal.end))
+        
+        threshold = "\(goal.goal.threshold)"
+        
+        switch goal.goal.metric {
+        case .income:
+            metric = .income
+            break
+        case .savings:
+            metric = .savings
+            break
+        case .spending:
+            metric = .spending
+            break
+        }
+        
+    }
+    
+    func edit(id: String) {
+        
+        errorDetail = ""
+        showAlert = false
+        
+        // Create a payload and pass it to the service.
+        // Just need to ensure all ints / doubles are valid and non-empty.
+        
+        if name.count == 0 {
+            alertType = .fail
+            errorDetail = "The name must be non-empty."
+            showAlert = true
+            return
+        }
+        
+        if threshold.count == 0 {
+            alertType = .fail
+            errorDetail = "The threshold field must be non-empty."
+            showAlert = true
+            return
+        }
+        
+        var thresholdParse: Double = 0
+        
+        if let parse = Double(threshold) {
+            thresholdParse = parse
+        }else {
+            alertType = .fail
+            errorDetail = "Failed to parse the threshold field. Please ensure it is a valid number."
+            showAlert = true
+            return
+        }
+        
+        if start >= end {
+            alertType = .fail
+            errorDetail = "The starting date must be strictly prior to the ending date."
+            showAlert = true
+            return
+        }
+        
+        let payload: GoalNewPayload = GoalNewPayload(name: name,
+                                                     start: Int64(start.timeIntervalSince1970),
+                                                     end: Int64(end.timeIntervalSince1970),
+                                                     threshold: thresholdParse,
+                                                     metric: metric.openAPI)
+        
+        GoalsService.updateGoal(id: id, payload: payload) { (success, error, _) in
+            DispatchQueue.main.async {
+                if success {
+                    // show a success alert and then from there set the presentation bool to false.
+                    self.alertType = .success
+                    self.showAlert = true
+                }else {
+                    if let error = error {
+                        self.errorDetail = "\(error)"
+                    }
+                    self.alertType = .fail
+                    self.showAlert = true
+                }
+            }
+            
+            
+        }
+        
+    }
+    
+}
