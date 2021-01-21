317f39bf75d8fb86553fd39f269d5efc4a127be5
Brett Fazio <brett.fazio@hotmail.com>
2021-01-21 10:25:05 -0500

    fix for goal edit?

--- finchios/Presentation/Goal/GoalEdit/GoalEditViewModel.swift
+++ finchios/Presentation/Goal/GoalEdit/GoalEditViewModel.swift
@@ -86,7 +86,7 @@ class GoalEditViewModel: ObservableObject, Identifiable {
         let payload: GoalNewPayload = GoalNewPayload(name: name,
                                                      start: Int64(start.timeIntervalSince1970),
                                                      end: Int64(end.timeIntervalSince1970),
-                                                     threshold: thresholdParse,
+                                                     threshold: thresholdParse*100,
                                                      metric: metric.openAPI)
         
         GoalsService.updateGoal(id: id, payload: payload) { (success, error, _) in
