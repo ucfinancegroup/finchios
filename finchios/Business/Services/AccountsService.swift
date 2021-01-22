e8c7bdd4328e54ddeead94e1ddf7299138985f7f
Brett Fazio <brett.fazio@hotmail.com>
2021-01-22 09:39:38 -0500

    stub for get accounts

---
+++ finchios/Business/Services/AccountsService.swift
@@ -0,0 +1,51 @@
+//
+//  AccountsService.swift
+//  finchios
+//
+//  Created by Brett Fazio on 1/22/21.
+//
+
+import Foundation
+import OpenAPIClient
+
+// GET /plaid/accounts
+public struct AccountsService {
+    
+    public static func accounts(completion: @escaping ((Bool, Error?, [String:String]?) -> Void)) {
+        guard let url = getAllAccountsURL() else {
+            completion(false, nil, nil)
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = ["Content-Type": "application/json",
+                                       BusinessConstants.SET_COOKIE : CredentialsObject.shared.jwt]
+
+        let task = URLSession.shared.dataTask(with: request) { (data, urlResponse, error) in
+            guard let data = data else {
+                completion(false, error, nil)
+                return
+            }
+            
+            guard let response = try? JSONDecoder().decode([String:String].self, from: data) else {
+                completion(false, error, nil)
+                return
+            }
+
+            completion(true, nil, response)
+            
+            return
+        }
+
+        task.resume()
+
+    }
+    
+    private static func getAllAccountsURL() -> URL? {
+        let address = "\(BusinessConstants.SERVER)/plaid/accounts"
+
+        return URL(string: address)
+    }
+    
+}
