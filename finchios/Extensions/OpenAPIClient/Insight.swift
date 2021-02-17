f99d061329f398589d3f945d24aa00632c81bb95
Brett Fazio <brett.fazio@hotmail.com>
2021-02-17 15:05:58 -0500

    update to latest spec

--- finchios/Extensions/OpenAPIClient/Insight.swift
+++ finchios/Extensions/OpenAPIClient/Insight.swift
@@ -10,6 +10,6 @@ import OpenAPIClient
 
 extension Insight {
     
-    static let empty = Insight(title: "", description: "", insightType: .goal, dismissed: false, generationTime: 0, imageURL: nil)
-    static let dummy = Insight(title: "A very cool insight.", description: "This insight has type goal", insightType: .goal, dismissed: false, generationTime: 0, imageURL: nil)
+    static let empty = Insight(id: .dummy, title: "", description: "", insightType: .goal, dismissed: false, generationTime: 0, imageURL: nil)
+    static let dummy = Insight(id: .dummy, title: "A very cool insight.", description: "This insight has type goal", insightType: .goal, dismissed: false, generationTime: 0, imageURL: nil)
 }
---
+++ finchios/Extensions/OpenAPIClient/MongoObjectID.swift
@@ -0,0 +1,15 @@
+//
+//  MongoObjectID.swift
+//  finchios
+//
+//  Created by Brett Fazio on 2/17/21.
+//
+
+import Foundation
+import OpenAPIClient
+
+extension MongoObjectID {
+    
+    static let dummy = MongoObjectID(oid: "1234abcd")
+    
+}
