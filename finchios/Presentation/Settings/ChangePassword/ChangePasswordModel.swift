f8149981d07221f256492d72eca23ff2b183d114
Brett Fazio <brett.fazio@hotmail.com>
2021-02-28 16:14:36 -0500

    change password

--- finchios/Presentation/Settings/ChangePassword/ChangePasswordModel.swift
+++ finchios/Presentation/Settings/ChangePassword/ChangePasswordModel.swift
@@ -6,7 +6,44 @@
 //
 
 import Foundation
+import OpenAPIClient
 
 class ChangePasswordModel: ObservableObject, Identifiable {
+    
+    enum ChangePasswordAlertType {
+        case notEqual, success, server
+    }
+    
+    @Published var newPassword: String = ""
+    @Published var confirmPassword: String = ""
+    
+    @Published var showAlert: Bool = false
+    @Published var type: ChangePasswordAlertType = .notEqual
+    
+    func changedTapped() {
+        if newPassword != confirmPassword {
+            type = .notEqual
+            showAlert = true
+            return
+        }
+        
+        let updatePassPayload = UpdateUserRequest(password: newPassword, firstName: nil, lastName: nil, income: nil)
+        
+        UserService.update(payload: updatePassPayload) { (success, _, user) in
+            DispatchQueue.main.async {
+                if let _ = user {
+                    self.type = .success
+                    
+                    let cacheJWT = CredentialsObject.shared.jwt
+                    let cacheEmail = CredentialsObject.shared.email
+                    
+                    _ = CredentialsObject.resetCredentials(jwt: cacheJWT, email: cacheEmail, password: self.newPassword)
+                }else {
+                    self.type = .server
+                }
+                self.showAlert = true
+            }
+        }
+    }
     
 }
--- finchios/Presentation/Settings/ChangePassword/ChangePasswordView.swift
+++ finchios/Presentation/Settings/ChangePassword/ChangePasswordView.swift
@@ -12,7 +12,53 @@ struct ChangePasswordView: View {
     @StateObject var model = ChangePasswordModel()
     
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            
+            SecureField("New Password", text: $model.newPassword)
+                .padding()
+            
+            Spacer()
+                .frame(height: 50)
+            
+            SecureField("Confirm New Password", text: $model.confirmPassword)
+                .padding()
+            
+            Spacer()
+            
+            Button(action: {
+                self.model.changedTapped()
+            }) {
+                Text("Change Password")
+                    .padding()
+                    .bubble(.teal)
+                    .foregroundColor(.white)
+            }
+            
+        }
+        .navigationTitle("Change Password")
+        .alert(isPresented: $model.showAlert) { () -> Alert in
+            if model.type == .notEqual {
+                return Alert(title: Text("Passwords do not match"),
+                             message: Text("The two passwords you entered do not match. Please confirm what was typed."),
+                             dismissButton: .default(Text("Okay")) {
+                                self.model.showAlert = false
+                             })
+            }
+            else if model.type == .success { // success
+                return Alert(title: Text("Success!"),
+                             message: Text("Your password has been successfully changed."),
+                             dismissButton: .default(Text("Okay")) {
+                                self.model.showAlert = false
+                             })
+            }
+            else {//if model.type == .server {
+                return Alert(title: Text("A server error occured."),
+                             message: Text("Please ensure you have a valid internet connection and try again."),
+                             dismissButton: .default(Text("Okay")) {
+                                self.model.showAlert = false
+                             })
+            }
+        }
     }
 }
 
