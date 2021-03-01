f0f29170c7fa529e665faa10d5a6dbe45f7d1a15
Brett Fazio <brett.fazio@hotmail.com>
2021-02-28 22:59:53 -0500

    asset service

---
+++ finchios/Business/Services/AssetService.swift
@@ -0,0 +1,49 @@
+//
+//  AssetService.swift
+//  finchios
+//
+//  Created by Brett Fazio on 2/28/21.
+//
+
+import Foundation
+import OpenAPIClient
+
+public struct AssetService {
+    
+    public func get(completion: @escaping ((Bool, Error?, [AssetClassAndApy]?) -> Void)) {
+        guard let url = getURL() else {
+            completion(false, nil, nil)
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        request.allHTTPHeaderFields = ["Content-Type": "application/json",
+                                       BusinessConstants.SET_COOKIE : CredentialsObject.shared.jwt]
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, urlResponse, error) in
+            guard let data = data else {
+                completion(false, error, nil)
+                return
+            }
+            
+            guard let response = try? JSONDecoder().decode([AssetClassAndApy].self, from: data) else {
+                completion(false, error, nil)
+                return
+            }
+            
+            completion(true, nil, response)
+            return
+        }
+
+        task.resume()
+    }
+    
+    private func getURL() -> URL? {
+        let address = "\(BusinessConstants.SERVER)/asset_classes"
+        
+        return URL(string: address)
+    }
+    
+}
