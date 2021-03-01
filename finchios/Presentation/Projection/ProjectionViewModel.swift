f44ea5efb2ac7c94a17451a697ef94ba60a11e90
Brett Fazio <brett.fazio@hotmail.com>
2021-02-28 21:33:04 -0500

    remove dead code from model

--- finchios/Presentation/Projection/ProjectionViewModel.swift
+++ finchios/Presentation/Projection/ProjectionViewModel.swift
@@ -10,28 +10,7 @@ import OpenAPIClient
 
 public class ProjectionViewModel: ObservableObject, Identifiable {
 
-    @Published public var insights: [Iden<Insight>] = []
-    
-    private var insightsGen: Bool = false
-    
     public func onAppear() {
-        self.fetchInsights()
-    }
-    
-    private func fetchInsights() {
-        if insightsGen {
-            return
-        }
-        insightsGen = true
-        
-        // Fetch Insights
-        InsightsService.example { (success, _, result) in
-            DispatchQueue.main.async {
-                if let result = result {
-                    self.insights = result.enumerated().map { Iden<Insight>(obj: $1, index: $0) }
-                }
-            }
-        }
     }
     
 }
