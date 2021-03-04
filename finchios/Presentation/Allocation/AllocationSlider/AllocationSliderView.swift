92099ec01d3c442b550e653a36093558bf7ad706
Brett Fazio <brett.fazio@hotmail.com>
2021-03-04 13:24:25 -0500

    allocation slider basic slider & menu

--- finchios/Presentation/Allocation/AllocationSlider/AllocationSliderView.swift
+++ finchios/Presentation/Allocation/AllocationSlider/AllocationSliderView.swift
@@ -6,25 +6,67 @@
 //
 
 import SwiftUI
+import OpenAPIClient
 
 struct AllocationSliderView: View {
+    
+    @Binding var value: Double
+    
+    @Binding var selection: AssetClass
+    
+    @Binding var classTypes: [Iden<AssetClass>]
+    
     var body: some View {
         VStack {
             HStack {
                 
-                Button {
-                    
+                Menu {
+                    ForEach(classTypes, id: \.id) { c in
+                        Button(action: {
+                            self.selection = c.obj
+                        }, label: {
+                            Text(c.obj.content)
+                        })
+                    }
                 } label: {
-                    Image(uiImage: UIImage(named: "TrashClear")!.withTintColor(.red))
+                    Text(selection.content)
                 }
+                Slider(value: $value, in: 0...100, step: 1)
+                
+                Text("\(Int(value))")
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(uiImage: UIImage(named: "TrashClear")!.withTintColor(.red))
+                })
+                .foregroundColor(.red)
+                
+
+                
                 
             }
+            .padding()
         }
     }
 }
 
+struct AllocationSliderViewPreview: View {
+    
+    @State var value: Double = 0.0
+    @State var aClass = AssetClass.init(typ: .equity, content: "Equity")
+    @State var classes = [
+        Iden<AssetClass>(obj: AssetClass.init(typ: .equity, content: "Equity")),
+        Iden<AssetClass>(obj: AssetClass.init(typ: .cash, content: "Cash"))]
+    
+    var body: some View {
+        AllocationSliderView(value: $value, selection: $aClass, classTypes: $classes)
+    }
+    
+}
+
 struct AllocationSliderView_Previews: PreviewProvider {
     static var previews: some View {
-        AllocationSliderView()
+        AllocationSliderViewPreview()
     }
 }
