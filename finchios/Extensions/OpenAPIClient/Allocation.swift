3856acc2ec8bf5c9e3901f695608f2ae7bb39748
Brett Fazio <brett.fazio@hotmail.com>
2021-03-04 21:25:52 -0500

    update allocation obj

--- finchios/Extensions/OpenAPIClient/Allocation.swift
+++ finchios/Extensions/OpenAPIClient/Allocation.swift
@@ -13,8 +13,8 @@ extension Allocation {
     static let dummy = Allocation(description: "My planned 25 year old allocation",
                                   date: 1614570311,
                                   schema: [
-                                    AllocationProportion(asset: .init(name: "Stock", _class: .init(typ: .equity, content: "Stock")), proportion: 50),
-                                    AllocationProportion(asset: .init(name: "Bond", _class: .init(typ: .fixed, content: "Bond")), proportion: 20),
-                                    AllocationProportion(asset: .init(name: "Cash", _class: .init(typ: .cash, content: "Cash")), proportion: 30)])
+                                    AllocationProportion(asset: .init(name: "Stock", _class: .init(typ: .equity, content: "Stock"), annualizedPerformance: 5), proportion: 50),
+                                    AllocationProportion(asset: .init(name: "Bond", _class: .init(typ: .fixed, content: "Bond"), annualizedPerformance: 1), proportion: 20),
+                                    AllocationProportion(asset: .init(name: "Cash", _class: .init(typ: .cash, content: "Cash"), annualizedPerformance: 0.1), proportion: 30)])
     
 }
