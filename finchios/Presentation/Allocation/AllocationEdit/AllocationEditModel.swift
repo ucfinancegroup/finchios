f212ad595c358bb32caca35185164c6ad6795d6d
Brett Fazio <brett.fazio@hotmail.com>
2021-03-16 20:25:17 -0400

    alloc edit model basics

--- finchios/Presentation/Allocation/AllocationEdit/AllocationEditModel.swift
+++ finchios/Presentation/Allocation/AllocationEdit/AllocationEditModel.swift
@@ -17,10 +17,46 @@ class AllocationEditModel: ObservableObject, Identifiable {
     @Published var errorString: String = ""
 
     // Fields
+    @Published var ids: [UUID] = []
+    @Published var classes: [UUID: (Iden<AssetClassAndApy>, Iden<Double>)] = [:]
     
+    @Published var classTypes: [Iden<AssetClassAndApy>] = []
+    
+    init() {
+        
+        AssetService.get { (success, error, result) in
+            DispatchQueue.main.async {
+                if let result = result {
+                    self.classTypes = result.map { Iden<AssetClassAndApy>(obj: $0) }
+                }
+            }
+        }
+        
+    }
+    
+    func getSum() -> Int {
+        
+        var sum = 0
+        
+        for id in ids {
+            if let obj = classes[id] {
+                sum += Int(obj.1.obj)
+            }
+        }
+        
+        return sum
+    }
+    
+    func getAllocationObject() -> Allocation? {
+        return nil
+    }
+    
+    func validateSum() -> Bool {
+        return getSum() == 100
+    }
     
     func set(alloc: Allocation) {
-        
+        // Set the classes & create IDs here.
     }
     
 }
