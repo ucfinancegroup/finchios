7a304384ac61f9bc56839a902dd37e4bddec58ff
Brett Fazio <brett.fazio@hotmail.com>
2021-03-17 21:41:24 -0400

    basic event detail

--- finchios/Presentation/Events/EventDetail/EventDetailModel.swift
+++ finchios/Presentation/Events/EventDetail/EventDetailModel.swift
@@ -6,7 +6,40 @@
 //
 
 import Foundation
+import OpenAPIClient
 
 class EventDetailModel: ObservableObject, Identifiable {
+    
+    @Published var showAlert: Bool = false
+    
+    @Published var success: Bool = false
+    
+    @Published var showError: Bool = false
+    @Published var errorString: String = ""
+    
+    func setup(event: Event) {
+        
+    }
+    
+    func delete(id: String) {
+        PlansService.deleteEvent(id: id) { (success, error, _, response) in
+            DispatchQueue.main.async {
+                self.success = false
+                self.showError = false
+                self.errorString = ""
+                
+                if success {
+                    self.success = true
+                }else {
+                    self.showError = true
+                    if let error = error {
+                        self.errorString = "\(error)"
+                    }
+                }
+                
+                self.showAlert = true
+            }
+        }
+    }
     
 }
